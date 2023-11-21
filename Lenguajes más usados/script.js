9e3b95c3783f27d89ef64a96d0cefb97dfd83bd7
alanromero781 <quitsilver54@gmail.com>
2023-11-21 17:23:01 -0500

    React JS

---
+++ Lenguajes más usados/script.js
@@ -0,0 +1,17 @@
+const searchBar = document.getElementById('searchBar');
+const sections = document.querySelectorAll('.languages');
+
+searchBar.addEventListener('input', (e) => {
+  const searchValue = e.target.value.toLowerCase();
+
+  sections.forEach(section => {
+    const sectionId = section.id.toLowerCase();
+    const sectionTitle = section.querySelector('h2').textContent.toLowerCase();
+
+    if (sectionId.includes(searchValue) || sectionTitle.includes(searchValue)) {
+      section.style.display = 'block';
+    } else {
+      section.style.display = 'none';
+    }
+  });
+});
